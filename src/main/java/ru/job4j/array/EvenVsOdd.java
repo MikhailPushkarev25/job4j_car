5639c15ac8f1f18d4b14dcba0f7ff34efebf24cd
mikha <mixa199525@yandex.ru>
2021-12-15 23:43:36 +0300

    5.8. Четные vs Нечетные

---
+++ src/main/java/ru/job4j/array/EvenVsOdd.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class EvenVsOdd {
+
+    public static int whoWin(int[] players) {
+        int EvenSize = 0, OddSize = 0, size = 0;
+       for (int i = 0; i < players.length; i++) {
+           if (i % 2 == 0) {
+               EvenSize += players[i];
+           } else {
+               OddSize += players[i];
+           }
+       }
+       if (EvenSize > OddSize) {
+           return 1;
+       } else if (OddSize > EvenSize) {
+           return 2;
+       }
+       return 0;
+    }
+}
